c7f4f09b540e7b7001cc12b273e33ddee55a7280
Tendy Developer <serendipity.10they@gmail.com>
2020-01-13 11:45:14 +0700

    fix modal and alert toast
    fix modal and alert toast

---
+++ application/controllers/Produk.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit ('No direct script alalowed');
+
+class Produk extends CI_Controller{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	function index(){
+		$this->load->helper('url');
+		$this->load->view('v_produk');
+	}
+}
