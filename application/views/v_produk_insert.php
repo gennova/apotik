7e3376363a3604a97e2651e4b6eb9ec068c550a2
Tendy Developer <serendipity.10they@gmail.com>
2020-01-14 14:13:58 +0700

    form update
    form update

---
+++ application/views/v_produk_insert.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<?php include('basehome/headdata.php'); ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-6">
+            <!-- general form elements -->
+
+            <!-- Form Element sizes -->
+            <div class="card card-success">
+              <div class="card-header" style="padding: 0.25rem 0.25rem">
+                <h3 class="card-title">Different Height</h3>
+              </div>
+              <div class="card-body" style="padding: 0.25rem">                
+                NAMA <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm">
+                NAMA <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm">
+                PILIH
+                        <select class="form-control form-control-sm">
+                          <option>option 1</option>
+                          <option>option 2</option>
+                          <option>option 3</option>
+                          <option>option 4</option>
+                          <option>option 5</option>
+                        </select>              
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            <div class="card card-danger">
+              <div class="card-header" style="padding: 0.25rem 0.25rem">
+                <h3 class="card-title">Different Width</h3>
+              </div>
+              <div class="card-body" style="padding: 0.25rem">
+                <div class="row">
+                  <div class="col-3">
+                    <input type="text" class="form-control form-control-sm" placeholder=".col-3">
+                  </div>
+                  <div class="col-4">
+                    <input type="text" class="form-control form-control-sm" placeholder=".col-4">
+                  </div>
+                  <div class="col-5">
+                        <select class="form-control form-control-sm">
+                          <option>option 1</option>
+                          <option>option 2</option>
+                          <option>option 3</option>
+                          <option>option 4</option>
+                          <option>option 5</option>
+                        </select>   
+                  </div>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            <!-- Horizontal Form -->
+        </div>
+          <div class="col-md-6">
+            <!-- general form elements -->
+
+            <!-- Form Element sizes -->
+            <div class="card card-success">
+              <div class="card-header" style="padding: 0.25rem 0.25rem">
+                <h3 class="card-title">Different Height</h3>
+              </div>
+              <div class="card-body" style="padding: 0.25rem">                
+                NAMA <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm">
+                NAMA <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm">
+                NAMA <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm">               
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            <div class="card card-danger">
+              <div class="card-header" style="padding: 0.25rem 0.25rem">
+                <h3 class="card-title">Different Width</h3>
+              </div>
+              <div class="card-body" style="padding: 0.25rem">
+                <div class="row">
+                  <div class="col-3">
+                    <input type="text" class="form-control form-control-sm" placeholder=".col-3">
+                  </div>
+                  <div class="col-4">
+                    <input type="text" class="form-control form-control-sm" placeholder=".col-4">
+                  </div>
+                  <div class="col-5">
+                    <input type="text" class="form-control form-control-sm" placeholder=".col-5">
+                  </div>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            <!-- Horizontal Form -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.0.0-rc.3
+    </div>
+    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
+    reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+</body>
+</html>
