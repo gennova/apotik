2dcf2fb0104e2cc8710dd98021e7e4db8ad579b1
Tendy Developer <serendipity.10they@gmail.com>
2020-01-24 19:30:54 +0700

    Update eceran menu
    eceran menu

--- application/views/basehome/homeheadnavaside.php
+++ application/views/basehome/homeheadnavaside.php
@@ -213,7 +213,7 @@
             </a>
               <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="pages/layout/top-nav.html" class="nav-link">
+                <a href="<?php echo base_url('eceran'); ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Eceran</p>
                 </a>
